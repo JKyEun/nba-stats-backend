2e3dcf09da2058245e6b78c958b20bd44e34b678
장경은 <j56237@naver.com>
2023-08-07 20:27:41 +0900

    feat: 네이버 스포츠를 통해 스탯 가져와서 보내주기

--- controllers/statController.js
+++ controllers/statController.js
@@ -3,17 +3,61 @@ const cheerio = require('cheerio');
 
 const getStats = async (req, res) => {
   try {
-    const url = 'https://www.melon.com/chart/';
+    const url =
+      'https://sports.news.naver.com/basketball/record/index.nhn?category=nba&year=2023';
     const stat = await axios.get(url);
-    const $ = cheerio.load(stat);
-    const name = $(
-      '#lst50 > td:nth-child(6) > div > div > div.ellipsis.rank01 > span > a',
-    ).text();
-    console.log(name);
-    res.status(200).send(name);
+
+    const $ = cheerio.load(stat.data);
+    const rank = $('#playerRecordTable').text();
+    const arr = rank
+      .trim()
+      .split('\n')
+      .map((el) => el.trim())
+      .filter((el) => el !== '');
+    const dataArr = [];
+    const eachPlayer = {};
+    for (let i = 0; i < 281; i++) {
+      if (i % 14 === 0) {
+        if (eachPlayer.name && eachPlayer.name.includes('트레')) {
+          eachPlayer.rank = 11;
+        } else if (eachPlayer.name && eachPlayer.name.includes('드로잔')) {
+          eachPlayer.rank = 18;
+        }
+        dataArr.push({ ...eachPlayer });
+        eachPlayer.rank = +arr[i];
+      } else if (i % 14 === 1) {
+        eachPlayer.name = arr[i];
+      } else if (i % 14 === 2) {
+        eachPlayer.games = +arr[i];
+      } else if (i % 14 === 3) {
+        eachPlayer.points = +arr[i];
+      } else if (i % 14 === 4) {
+        eachPlayer.assist = +arr[i];
+      } else if (i % 14 === 5) {
+        eachPlayer.rebound = +arr[i];
+      } else if (i % 14 === 6) {
+        eachPlayer.steal = +arr[i];
+      } else if (i % 14 === 7) {
+        eachPlayer.block = +arr[i];
+      } else if (i % 14 === 8) {
+        eachPlayer.fgNum = +arr[i];
+      } else if (i % 14 === 9) {
+        eachPlayer.pt3Num = +arr[i];
+      } else if (i % 14 === 10) {
+        eachPlayer.ftNum = +arr[i];
+      } else if (i % 14 === 11) {
+        eachPlayer.fgRate = +arr[i];
+      } else if (i % 14 === 12) {
+        eachPlayer.pt3Rate = +arr[i];
+      } else if (i % 14 === 13) {
+        eachPlayer.ftRate = +arr[i];
+      }
+    }
+    dataArr.shift();
+    res.status(200).send(dataArr);
   } catch (err) {
     console.error(err);
-    res.status(500).send('500번 에러입니다.');
+    res.status(500).send('Error 500.');
   }
 };
 
